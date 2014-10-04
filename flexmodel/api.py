e71518d5099c5a759682fc516cb8cc1bd597491d
Roman Tolkachyov <r.tolkachyov@dinect.com>
2014-10-04 20:54:03 +0700

    make a base class for api views (for future :)

--- flexmodel/api.py
+++ flexmodel/api.py
@@ -5,10 +5,14 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
 import models
 
-# class TestView(generics.ListCreateAPIView):
-#     model = MyModel
-#     serializer_class = UserSerializer
-#     paginate_by = 100
+
+class FlexListAPIView(ListCreateAPIView):
+    pass
+
+
+class FlexDetailAPIView(RetrieveUpdateAPIView):
+    pass
+
 
 urlpatterns = []
 
@@ -16,11 +20,11 @@ for model_name in models.flex_model_list:
     model = getattr(models, model_name)
     attrs = dict(model=model, paginate_by=50)
     # TODO: restframework routing, viewset
-    list_api = type("%sList" % model_name, (ListCreateAPIView,), attrs)
+    list_api = type("%sList" % model_name, (FlexListAPIView,), attrs)
     list_api_url = url(r"^api/%s$" % model_name, list_api.as_view())
     urlpatterns.append(list_api_url)
 
     attrs = dict(model=model)
-    detail_api = type("%sDetail" % model_name, (RetrieveUpdateAPIView,), attrs)
+    detail_api = type("%sDetail" % model_name, (FlexDetailAPIView,), attrs)
     detail_api_url = url(r"^api/%s/(?P<pk>\d+)$" % model_name, detail_api.as_view())
     urlpatterns.append(detail_api_url)
